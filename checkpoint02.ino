d55e4c65383b895c2bb975d33f6e28887627ab7a
arduinoteam20 <lancer856618@gmail.com>
2017-05-01 18:57:40 +0800

    Add files via upload

---
+++ checkpoint02.ino
@@ -0,0 +1,19 @@
+#include <LiquidCrystal.h> // include the library code
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); // initialize interface pins
+void setup() 
+{
+  lcd.begin(20, 2); // set up the LCD's number of columns and rows:
+  Serial.begin(9600);
+}
+void loop() 
+{
+ if(Serial.available())
+  {
+   delay(1000);
+   lcd.clear();
+   while(Serial.available()>0)
+   {
+    lcd.write(Serial.read());
+    }
+  } 
+}
